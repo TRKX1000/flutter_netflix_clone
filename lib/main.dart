551d2cdbcfcbcdf4aceb78b4eabc79b850acc66d
Luis Barroso Vázquez <e_lbarro@mercadona.es>
2021-12-27 13:09:42 +0100

    ADD - navigation to home page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_netflix_clone/router/app_router.dart';
 
-import 'pages/userSelection/user_selection.dart';
+import 'data/user.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  User? userSelected;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Netflix Clone',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const UserSelectionPage(),
+    return const MaterialApp(
+      home: AppRouter(),
     );
   }
 }
